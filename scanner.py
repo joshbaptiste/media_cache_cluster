a607a02aaef996bf37bbe073a9002ea04dcd6780
Reynold Chery <joshbaptiste@yahoo.com>
2018-03-20 09:51:44 -0400

    updates

--- scanner.py
+++ scanner.py
@@ -1,19 +1,26 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import os
 
 class Scanner:
-    # Scans directories and generates dir entries
+    "Scans directories and generates dir entries"
 
-    def __init__():
+    def __init__(self):
         REMOTE_DIR = config.REMOTE_DIR
         CACHE_DIR = config.CACHED_DIR
     
-    def scanner(self, path):
+    def scanDirectory(self, path):
         with os.scandir(path) as files:
             for fn in files:
-                if not fn.name.startswith('.') and fn.is_file():
-                    print(fn.name)
+                if fn.is_file():
+                    yield fn.name
 
-    def createSymLinks(self):
+    def createSymLinks(self, path, fn):
+        os.symlink(fn, CACHE_DIR)
+
+    def removeSymLink(self):
         pass
+
+    def updateSymLink(self):
+        pass
+
